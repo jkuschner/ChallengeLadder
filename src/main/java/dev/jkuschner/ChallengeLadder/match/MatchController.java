135bd3bbc84b362c98027a93354d5f30a577e587
Joshua <jkuschne@ucsd.edu>
2025-01-17 01:46:59 -0800

    add basic CRUD operations for match API for hardcoded data

--- src/main/java/dev/jkuschner/ChallengeLadder/match/MatchController.java
+++ src/main/java/dev/jkuschner/ChallengeLadder/match/MatchController.java
@@ -1,8 +1,12 @@
 package dev.jkuschner.ChallengeLadder.match;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/matches")
@@ -14,11 +18,42 @@ public class MatchController {
        this.matchRepository = matchRepository;
     }
 
+    @GetMapping("")
+    List<Match> findAll() {
+        return matchRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Match findById(@PathVariable Integer id) {
+        Optional<Match> match = matchRepository.findById(id);
+        if (match.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return match.get();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@Valid @RequestBody Match match) {
+        matchRepository.create(match);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@Valid @RequestBody Match match, @PathVariable Integer id) {
+        matchRepository.update(match, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        matchRepository.delete(id);
+    }
+
     @GetMapping("/hello")
     String home() {
         return "Hello there";
     }
 
 
-    //populate with CRUD methods for match data
 }
--- src/main/java/dev/jkuschner/ChallengeLadder/match/MatchRepository.java
+++ src/main/java/dev/jkuschner/ChallengeLadder/match/MatchRepository.java
@@ -4,13 +4,35 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MatchRepository {
 
     private List<Match> matches = new ArrayList<>();
 
-    List<Match> findAll() {
+    public List<Match> findAll() {
         return matches;
+    }
+
+    public Optional<Match> findById(Integer id) {
+        return matches.stream()
+                .filter(match -> match.id().equals(id))
+                .findFirst();
+    }
+
+    public void create(Match match) {
+        matches.add(match);
+    }
+
+    public void update(Match match, Integer id) {
+        Optional<Match> existingMatch = findById(id);
+        if(existingMatch.isPresent()) {
+            matches.set(matches.indexOf(existingMatch.get()), match);
+        }
+    }
+
+    public void delete(Integer id) {
+        matches.removeIf(match -> match.id().equals(id));
     }
 }

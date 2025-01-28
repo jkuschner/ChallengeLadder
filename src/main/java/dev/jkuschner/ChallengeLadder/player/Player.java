c59485e8365f53d0d17eaf92f79ef0506eef500f
jkuschner <joshua.kuschner@gmail.com>
2025-01-27 18:16:44 -0800

    update schema.sql to include Players table and link player id's in Matches table to Players table; remove match_history field from Player record

--- src/main/java/dev/jkuschner/ChallengeLadder/player/Player.java
+++ src/main/java/dev/jkuschner/ChallengeLadder/player/Player.java
@@ -1,13 +1,10 @@
 package dev.jkuschner.ChallengeLadder.player;
 
 import jakarta.validation.constraints.Positive;
-import dev.jkuschner.ChallengeLadder.match.Match;
-import java.util.List;
 
 public record Player(
         @Positive
         Integer id,
-        String name,
-        List<Match> matchHistory
+        String name
 ) {
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,9 @@
+CREATE TABLE IF NOT EXISTS Players (
+    id INT NOT NULL,
+    name varchar(100),
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE IF NOT EXISTS Matches (
     id INT NOT NULL,
     player1 INT NOT NULL,
@@ -5,5 +11,7 @@ CREATE TABLE IF NOT EXISTS Matches (
     match_date timestamp NOT NULL,
     score varchar(100) NOT NULL,
     winner INT NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (player1) REFERENCES Players(id),
+    FOREIGN KEY (player2) REFERENCES Players(id)
 );

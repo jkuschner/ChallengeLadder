d07d42e5073cdc5b330517d47a9e1a35c754cf09
jkuschner <joshua.kuschner@gmail.com>
2025-01-27 17:47:41 -0800

    add PlayerController with methods mapped to CRUD operations that implement PlayerRepository methods

--- src/main/java/dev/jkuschner/ChallengeLadder/player/PlayerController.java
+++ src/main/java/dev/jkuschner/ChallengeLadder/player/PlayerController.java
@@ -1,2 +1,52 @@
-package dev.jkuschner.ChallengeLadder.player;public class PlayerController {
+package dev.jkuschner.ChallengeLadder.player;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/players")
+public class PlayerController {
+
+    private final PlayerRepository playerRepository;
+
+    public PlayerController(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @GetMapping("")
+    List<Player> findAll() {
+        return playerRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Player findById(@PathVariable Integer id) {
+        Optional<Player> player = playerRepository.findById(id);
+        if (player.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return player.get();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@Valid @RequestBody Player player) {
+        playerRepository.create(player);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@Valid @RequestBody Player player, @PathVariable Integer id) {
+        playerRepository.update(player, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        playerRepository.delete(id);
+    }
 }
